1619c533e9e2e5c495b222b0ceafd4144cc82133
thllz <thllz@thllz-dsk.localdomain>
2022-10-07 20:23:07 -0300

    ,

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ class NotFound extends React.Component {
   render() {
     return (
       <div>
-        <h1>Página não encontrada!</h1>
+        <h1>Página não encontrada</h1>
         <img
           src="https://media2.giphy.com/media/C21GGDOpKT6Z4VuXyn/200w.webp?cid=ecf05e47h2ykzmdjhd21lh0b0r2lxraaiylm44yhlpr0p7s1&rid=200w.webp&ct=g"
           alt="PageNotFoundGif"
